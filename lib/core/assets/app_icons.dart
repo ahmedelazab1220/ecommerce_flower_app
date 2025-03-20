e6d2909762403c8257f166a2684813b64aab86a6
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-03-20 07:18:45 +0200

    feat(init): project setup with dependencies, assets, localization, theming, and CI/CD workflows.

---
+++ lib/core/assets/app_icons.dart
@@ -0,0 +1,19 @@
+class AppIcons {
+  static const String baseRoute = 'assets/svgs/';
+  static const String alarmClockSvg = '${baseRoute}alarm_clock.svg';
+  static const String callSvg = '${baseRoute}call.svg';
+  static const String categoriesSvg = '${baseRoute}categories.svg';
+  static const String chatSvg = '${baseRoute}chat.svg';
+  static const String diamondSvg = '${baseRoute}diamond.svg';
+  static const String errorSvg = '${baseRoute}error.svg';
+  static const String flowerSvg = '${baseRoute}flower.svg';
+  static const String heartEyesSvg = '${baseRoute}heart_eyes.svg';
+  static const String homeSvg = '${baseRoute}home.svg';
+  static const String locationSvg = '${baseRoute}location.svg';
+  static const String logoSvg = '${baseRoute}logo.svg';
+  static const String personSvg = '${baseRoute}person.svg';
+  static const String searchSvg = '${baseRoute}search.svg';
+  static const String shoppingCartSvg = '${baseRoute}shopping_cart.svg';
+  static const String starSvg = '${baseRoute}star.svg';
+  static const String translateSvg = '${baseRoute}translate.svg';
+}
