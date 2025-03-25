ed9cd3cd3407e744613f98fef1f14bc731e01733
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-03-25 21:42:38 +0200

    feat(auth): refactor AuthRepo to support multiple tokens with key-value pairs

--- lib/features/auth/data/repo_impl/auth_repo_impl.dart
+++ lib/features/auth/data/repo_impl/auth_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_flower_app/core/utils/datasource_excution/api_result.dart';
+import 'package:ecommerce_flower_app/features/auth/data/data_source/contract/auth_local_data_source.dart';
 import 'package:ecommerce_flower_app/features/auth/data/model/login/login_response_dto.dart';
 import 'package:ecommerce_flower_app/features/auth/domain/repo/auth_repo.dart';
 import 'package:injectable/injectable.dart';
@@ -10,25 +11,24 @@ import '../data_source/contract/auth_remote_data_source.dart';
 class AuthRepoImpl extends AuthRepo {
   final ApiManager _apiManager;
   final AuthRemoteDataSource _authRemoteDataSource;
+  final AuthLocalDataSource _authLocalDataSource;
 
-  AuthRepoImpl(this._authRemoteDataSource, this._apiManager);
+  AuthRepoImpl(
+      this._authRemoteDataSource, this._apiManager, this._authLocalDataSource);
 
   @override
-  Future<void> saveToken(String token) {
-    // TODO: implement saveToken
-    throw UnimplementedError();
+  Future<void> saveToken(String key, String value) async {
+    return await _authLocalDataSource.saveToken(key, value);
   }
 
   @override
-  Future<String?> getToken() {
-    // TODO: implement getToken
-    throw UnimplementedError();
+  Future<String?> getToken(String key) async {
+    return await _authLocalDataSource.getToken(key);
   }
 
   @override
-  Future<void> deleteToken() {
-    // TODO: implement deleteToken
-    throw UnimplementedError();
+  Future<void> deleteToken(String key) async {
+    return await _authLocalDataSource.deleteToken(key);
   }
 
   @override
--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -3,7 +3,10 @@ import '../../data/model/login/login_response_dto.dart';
 
 abstract class AuthRepo {
   Future<Result<LoginResponseDto>> login(String email, String password);
-  Future<void> saveToken(String token);
-  Future<String?> getToken();
-  Future<void> deleteToken();
+
+  Future<void> saveToken(String key, String value);
+
+  Future<String?> getToken(String key);
+
+  Future<void> deleteToken(String key);
 }
