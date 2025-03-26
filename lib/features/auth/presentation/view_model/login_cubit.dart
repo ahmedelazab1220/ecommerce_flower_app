8dde12d5b92fbfd25d97502de2445d2e3326e923
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-03-26 04:03:42 +0200

    feat(auth): implement LoginCubit and LoginState for user authentication

---
+++ lib/features/auth/presentation/view_model/login_cubit.dart
@@ -0,0 +1,46 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:injectable/injectable.dart';
+import 'package:meta/meta.dart';
+import '../../../../core/utils/datasource_excution/api_result.dart';
+import '../../../../core/utils/validator/validator.dart';
+import '../../domain/usecase/login_use_case.dart';
+
+part 'login_state.dart';
+
+@injectable
+class LoginCubit extends Cubit<LoginState> {
+  final LoginUseCase _loginUseCase;
+
+  final Validator _validator = Validator();
+
+  LoginCubit(this._loginUseCase) : super(LoginInitial());
+
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  Future<void> login() async {
+    if (formKey.currentState!.validate()) {
+      emit(LoginLoading());
+      final result = await _loginUseCase.call(
+          emailController.text, passwordController.text);
+      switch (result) {
+        case SuccessResult<void>():
+          emit(LoginSuccess());
+        case FailureResult<void>():
+          emit(LoginFailure(result.exception.toString()));
+      }
+    }
+  }
+
+  String? validateEmail(String value) {
+    return _validator.validateEmail(value);
+  }
+
+  String? validatePassword(String value) {
+    return _validator.validatePassword(value);
+  }
+}
