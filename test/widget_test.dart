2f221789f4a41ace6ed9377360550eb0ab2bbc2d
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-03-27 01:47:46 +0200

    refactor(widget_test): remove unnecessary comments and streamline test setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 import 'package:easy_localization/easy_localization.dart';
 import 'package:ecommerce_flower_app/core/utils/constants.dart';
 import 'package:ecommerce_flower_app/main.dart';
@@ -36,7 +30,6 @@ void main() {
   testWidgets('Finds main widget and localization', (
     WidgetTester tester,
   ) async {
-    // Pump the real app widget
     await pumpLocalizedWidget(tester);
   });
 }
