302b2adaffd8f7f5a8fd93328590796598d92fd8
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-04-04 22:47:55 +0200

    refactor(auth_repo): update save token to avoid future bugs

--- lib/features/auth/data/repo_impl/auth_repo_impl.dart
+++ lib/features/auth/data/repo_impl/auth_repo_impl.dart
@@ -16,21 +16,23 @@ class AuthRepoImpl extends AuthRepo {
   final AuthLocalDataSource _authLocalDataSource;
 
   AuthRepoImpl(
-      this._authRemoteDataSource, this._authLocalDataSource, this._apiManager);
+    this._authRemoteDataSource,
+    this._authLocalDataSource,
+    this._apiManager,
+  );
 
   @override
   Future<Result<void>> login(LoginRequest request) {
-    var response = _apiManager.execute<LoginResponseDto>(
-      () async {
-        final response = await _authRemoteDataSource.login(LoginRequestDto(
-          email: request.email,
-          password: request.password,
-        ));
+    var response = _apiManager.execute<LoginResponseDto>(() async {
+      final response = await _authRemoteDataSource.login(
+        LoginRequestDto(email: request.email, password: request.password),
+      );
+      if (response is SuccessResult<LoginResponseDto>) {
         _authLocalDataSource.saveToken("token", response.token ?? "");
         _authLocalDataSource.setRememberMe(request.isRememberMe);
-        return response;
-      },
-    );
+      }
+      return response;
+    });
     return response;
   }
 }
