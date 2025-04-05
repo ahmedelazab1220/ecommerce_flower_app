88acbccd6d2bebe69c41d329013cbabd0cd7b85e
Eslam <eslamm378@gmail.com>
2025-04-05 18:10:11 +0200

    feat(core, most_selling_products): Integrate best selling products feature
    - Updated Android build configuration to specify NDK version.
    - Added route for best seller screen in the app.
    - Set initial route to best seller screen for testing.
    - Registered dependencies for best seller feature using dependency injection.

--- lib/core/utils/di/di.config.dart
+++ lib/core/utils/di/di.config.dart
@@ -18,6 +18,20 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart' as _i528;
 import '../../../features/auth/data/api/auth_retorfit_client.dart' as _i257;
 import '../../../features/main_layout/presentation/view_model/cubit/main_layout_cubit.dart'
     as _i393;
+import '../../../features/most_selling_products/data/api/best_seller_retrofit_client.dart'
+    as _i821;
+import '../../../features/most_selling_products/data/data_source/contract/best_seller_remote_data_source.dart'
+    as _i488;
+import '../../../features/most_selling_products/data/data_source/remote/best_seller_remote_data_source_impl.dart'
+    as _i461;
+import '../../../features/most_selling_products/data/repo_impl/best_seller_repo_impl.dart'
+    as _i688;
+import '../../../features/most_selling_products/domain/repo/best_seller_repo.dart'
+    as _i58;
+import '../../../features/most_selling_products/domain/usecase/get_best_sellers_usecase.dart'
+    as _i109;
+import '../../../features/most_selling_products/presentation/view_model/best_seller_cubit.dart'
+    as _i190;
 import '../bloc_observer/bloc_observer_service.dart' as _i649;
 import '../datasource_excution/api_manager.dart' as _i28;
 import '../datasource_excution/dio_module.dart' as _i953;
@@ -26,12 +40,16 @@ import '../logging/logger_module.dart' as _i470;
 import '../validator/validator.dart' as _i468;
 
 extension GetItInjectableX on _i174.GetIt {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
     final dioModule = _$DioModule();
     final secureStorageModule = _$SecureStorageModule();
     final loggerModule = _$LoggerModule();
@@ -39,20 +57,27 @@ extension GetItInjectableX on _i174.GetIt {
     gh.singleton<_i393.MainLayoutCubit>(() => _i393.MainLayoutCubit());
     gh.lazySingleton<_i361.Dio>(() => dioModule.provideDio());
     gh.lazySingleton<_i528.PrettyDioLogger>(
-      () => dioModule.providerInterceptor(),
-    );
+        () => dioModule.providerInterceptor());
     gh.lazySingleton<_i558.FlutterSecureStorage>(
-      () => secureStorageModule.storage,
-    );
+        () => secureStorageModule.storage);
     gh.lazySingleton<_i974.Logger>(() => loggerModule.loggerProvider);
     gh.lazySingleton<_i974.PrettyPrinter>(() => loggerModule.prettyPrinter);
     gh.lazySingleton<_i468.Validator>(() => _i468.Validator());
     gh.singleton<_i649.BlocObserverService>(
-      () => _i649.BlocObserverService(gh<_i974.Logger>()),
-    );
+        () => _i649.BlocObserverService(gh<_i974.Logger>()));
     gh.singleton<_i257.AuthRetrofitClient>(
-      () => _i257.AuthRetrofitClient(gh<_i361.Dio>()),
-    );
+        () => _i257.AuthRetrofitClient(gh<_i361.Dio>()));
+    gh.singleton<_i821.BestSellerRetrofitClient>(
+        () => _i821.BestSellerRetrofitClient(gh<_i361.Dio>()));
+    gh.factory<_i488.BestSellerRemoteDataSource>(() =>
+        _i461.BestSellerRemoteDataSourceImpl(
+            bestSellerRetrofitClient: gh<_i821.BestSellerRetrofitClient>()));
+    gh.factory<_i58.BestSellerRepo>(() => _i688.BestSellerRepoImpl(
+        bestSellerRemoteDataSource: gh<_i488.BestSellerRemoteDataSource>()));
+    gh.factory<_i109.GetBestSellersUsecase>(() =>
+        _i109.GetBestSellersUsecase(bestSellerRepo: gh<_i58.BestSellerRepo>()));
+    gh.factory<_i190.BestSellerCubit>(
+        () => _i190.BestSellerCubit(gh<_i109.GetBestSellersUsecase>()));
     return this;
   }
 }
--- lib/core/utils/routes/routes.dart
+++ lib/core/utils/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_flower_app/features/most_selling_products/presentation/view/screens/best_sellers.dart';
 import 'package:flutter/material.dart';
 
 import '../../../features/main_layout/presentation/view/main_layout.dart';
@@ -7,8 +8,10 @@ class AppRoutes {
 
   static const String initialRoute = '/';
   static const String mainLayoutRoute = '/main-layout';
+  static const String bestSellerRoute = '/best-seller';
 
   static Map<String, Widget Function(BuildContext context)> routes = {
     mainLayoutRoute: (context) => const MainLayout(),
+    bestSellerRoute: (context) => BestSellers(),
   };
 }
