1bd94bf4ca264dedcab992bcc17770028fd68a80
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-04-17 16:20:17 +0200

    feat(cart): refactor cart item layout to improve structure and add skeleton loading for delete icon

--- lib/features/cart/presentation/view/widgets/cart_item.dart
+++ lib/features/cart/presentation/view/widgets/cart_item.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lottie/lottie.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../core/assets/app_lotties.dart';
 import '../../../../../core/utils/l10n/locale_keys.g.dart';
@@ -27,112 +28,118 @@ class CartItem extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Container(
-                width: 100,
-                color: AppColors.lightPink,
-                child: CachedNetworkImage(
-                  imageUrl: cartProductEntity.imgCover,
-                  fit: BoxFit.cover,
-                  errorWidget:
-                      (context, url, error) => const Icon(
-                        Icons.error,
-                        color: AppColors.pink,
-                        size: 40,
-                      ),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Container(
+                  width: 100,
+                  color: AppColors.lightPink,
+                  child: CachedNetworkImage(
+                    imageUrl: cartProductEntity.imgCover,
+                    fit: BoxFit.cover,
+                    errorWidget:
+                        (context, url, error) => const Icon(
+                          Icons.error,
+                          color: AppColors.pink,
+                          size: 40,
+                        ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 8),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                cartProductEntity.title,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                cartProductEntity.description,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                                style: const TextStyle(fontSize: 13),
+                              ),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            context.read<CartCubit>().doIntent(
+                              DeleteProductFromCartAction(cartProductEntity.id),
+                            );
+                          },
+                          icon: Skeleton.ignore(
+                            child: SvgPicture.asset(AppIcons.trashSvg),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '${LocaleKeys.EGP.tr()} : ${cartProductEntity.price * cartProductEntity.quantity}',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Row(
                           children: [
+                            IconButton(
+                              onPressed: () {
+                                if (cartProductEntity.quantity > 1) {
+                                  context.read<CartCubit>().doIntent(
+                                    UpdateProductInCartAction(
+                                      cartProductEntity.id,
+                                      cartProductEntity.quantity - 1,
+                                    ),
+                                  );
+                                }
+                              },
+                              icon: const Icon(Icons.remove),
+                            ),
                             Text(
-                              cartProductEntity.title,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
+                              cartProductEntity.quantity.toString(),
                               style: const TextStyle(
                                 fontWeight: FontWeight.bold,
-                                fontSize: 16,
                               ),
                             ),
-                            const SizedBox(height: 4),
-                            Text(
-                              cartProductEntity.description,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: const TextStyle(fontSize: 13),
-                            ),
-                          ],
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          context.read<CartCubit>().doIntent(
-                            DeleteProductFromCartAction(cartProductEntity.id),
-                          );
-                        },
-                        icon: SvgPicture.asset(AppIcons.trashSvg),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${LocaleKeys.EGP.tr()} : ${cartProductEntity.price * cartProductEntity.quantity}',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              if (cartProductEntity.quantity > 1) {
+                            IconButton(
+                              onPressed: () {
                                 context.read<CartCubit>().doIntent(
                                   UpdateProductInCartAction(
                                     cartProductEntity.id,
-                                    cartProductEntity.quantity - 1,
+                                    cartProductEntity.quantity + 1,
                                   ),
                                 );
-                              }
-                            },
-                            icon: const Icon(Icons.remove),
-                          ),
-                          Text(
-                            cartProductEntity.quantity.toString(),
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              context.read<CartCubit>().doIntent(
-                                UpdateProductInCartAction(
-                                  cartProductEntity.id,
-                                  cartProductEntity.quantity + 1,
-                                ),
-                              );
-                            },
-                            icon: const Icon(Icons.add),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                              },
+                              icon: const Icon(Icons.add),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
