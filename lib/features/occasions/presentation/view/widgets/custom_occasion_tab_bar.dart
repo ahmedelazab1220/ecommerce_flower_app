dd706bb7ce1e1842111e01e5b92e9b16a7682e96
Ammar Ahmed <ammare.ba720.com>
2025-04-18 02:23:43 +0200

    refactor(occasion): improve state management and UI handling in OccasionCubit and related components

--- lib/features/occasions/presentation/view/widgets/custom_occasion_tab_bar.dart
+++ lib/features/occasions/presentation/view/widgets/custom_occasion_tab_bar.dart
@@ -29,7 +29,7 @@ class CustomOccasionTabBar extends StatelessWidget {
                   }
                 },
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
                   child: IntrinsicWidth(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
--- lib/features/occasions/presentation/view/widgets/product_view.dart
+++ lib/features/occasions/presentation/view/widgets/product_view.dart
@@ -1,17 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:ecommerce_flower_app/core/utils/shared_models/product_entity.dart';
+import 'package:ecommerce_flower_app/core/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../core/base/base_state.dart';
 import '../../../../../core/utils/l10n/locale_keys.g.dart';
 import '../../../../../core/utils/routes/routes.dart';
 import '../../view_model/occasion_cubit.dart';
-import 'product_item.dart';
 
 class ProductView extends StatefulWidget {
-  final String occasionId;
-
-  const ProductView({super.key, required this.occasionId});
+  const ProductView({super.key});
 
   @override
   State<ProductView> createState() => _ProductViewState();
@@ -19,72 +19,56 @@ class ProductView extends StatefulWidget {
 
 class _ProductViewState extends State<ProductView> {
   @override
-  void initState() {
-    super.initState();
-    context.read<OccasionCubit>().doIntent(
-      ProductsRequestAction(widget.occasionId),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final viewModel = context.read<OccasionCubit>();
-
     return BlocBuilder<OccasionCubit, OccasionState>(
       builder: (context, state) {
-        final isLoading = state.baseState is BaseLoadingState;
-
-        if (state.baseState is BaseErrorState) {
-          return Center(
-            child: Text(
-              (state.baseState as BaseErrorState).errorMessage,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          );
-        }
-
-        if (!isLoading &&
-            (viewModel.products == null || viewModel.products!.isEmpty)) {
-          return Center(
-            child: Text(
-              LocaleKeys.NoProductsAvailable.tr(),
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          );
-        }
-
-        final itemCount = isLoading ? 6 : viewModel.products!.length;
-
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 17,
-              mainAxisSpacing: 17,
-              childAspectRatio: 0.74,
-            ),
-            itemCount: itemCount,
-            itemBuilder: (context, index) {
-              final product = isLoading ? null : viewModel.products![index];
-              return GestureDetector(
-                onTap: () {
-                  if (!isLoading && product != null) {
-                    Navigator.pushNamed(
-                      context,
-                      AppRoutes.productDetailsRoute,
-                      arguments: product,
-                    );
-                  }
-                },
-                child: ProductItem(
-                  productEntity: product,
-                  isLoading: isLoading,
+        final productsState = state.productsState;
+        final occasionState = state.occasionState;
+        final isError = productsState is BaseErrorState;
+        if (isError) {
+          return Center(child: Text((productsState).errorMessage));
+        } else {
+          final isLoading =
+              productsState is BaseLoadingState ||
+              occasionState is BaseLoadingState;
+          final products =
+              productsState is BaseSuccessState<List<ProductEntity>>
+                  ? productsState.data
+                  : List.generate(
+                    6,
+                    (_) => ProductEntity(
+                      title:
+                          LocaleKeys
+                              .NoProductsAvailable.tr(), //assign title to to increase the length of text in skeletonizer
+                    ),
+                  );
+          return products == null && products!.isEmpty
+              ? Center(child: Text(LocaleKeys.NoProductsAvailable.tr()))
+              : GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 17,
+                  mainAxisSpacing: 17,
+                  childAspectRatio: 0.8,
                 ),
+                itemBuilder: (context, index) {
+                  return Skeletonizer(
+                    enabled: isLoading,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          AppRoutes.productDetailsRoute,
+                          arguments: products[index],
+                        );
+                      },
+                      child: ProductItem(product: products[index]),
+                    ),
+                  );
+                },
               );
-            },
-          ),
-        );
+        }
       },
     );
   }
