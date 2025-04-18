dfae66ebeb364f227860aea0b75ab614800f9039
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-04-18 13:33:21 +0200

    refactor(cart): improve JSON serialization and deserialization for cart-related DTOs

--- lib/core/utils/l10n/locale_keys.g.dart
+++ lib/core/utils/l10n/locale_keys.g.dart
@@ -184,7 +184,6 @@ abstract class LocaleKeys {
   static const InvalidName = 'InvalidName';
   static const PhoneNumberCannotBeEmpty = 'PhoneNumberCannotBeEmpty';
   static const InvalidPhoneNumber = 'InvalidPhoneNumber';
-  static const NoProductsAvailable = 'NoProductsAvailable';
   static const Egp = 'Egp';
   static const all = 'all';
   static const FetchingYourLocation = 'FetchingYourLocation';
@@ -195,8 +194,14 @@ abstract class LocaleKeys {
   static const UnableToGetLocation = 'UnableToGetLocation';
   static const Long = 'Long';
   static const Lat = 'Lat';
+  static const NoProductsAvailable = 'NoProductsAvailable';
   static const EditProfile = 'EditProfile';
   static const TakeAPhoto = 'TakeAPhoto';
   static const ChooseFromGallery = 'ChooseFromGallery';
   static const Change = 'Change';
+  static const ProductAddedToCartSuccessfully =
+      'ProductAddedToCartSuccessfully';
+  static const YouAreInGuestMode = 'YouAreInGuestMode';
+  static const PleaseLoginToAccessAllFeatures =
+      'PleaseLoginToAccessAllFeatures';
 }
