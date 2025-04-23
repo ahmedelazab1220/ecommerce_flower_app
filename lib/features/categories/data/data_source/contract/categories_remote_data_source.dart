738120dcd7f80a3a3a77e207e7c36f4151ea68ce
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-04-23 21:21:54 +0200

    refactor(categories_remote_data_source): remove redundant parameter declarations in getProductsByCategory method

--- lib/features/categories/data/data_source/contract/categories_remote_data_source.dart
+++ lib/features/categories/data/data_source/contract/categories_remote_data_source.dart
@@ -4,11 +4,7 @@ import '../../models/products_response/product_dto.dart';
 abstract class CategoriesRemoteDataSource {
   Future<List<CategoryDto>> getCategories();
   Future<List<ProductDto>> getProductsByCategory({
-    
     String? categoryId,
-    int? price,
-    String? sort,
-  ,
     int? price,
     String? sort,
   });
