73b29ef6baecae49b097e0bd277043d4e00931d8
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2025-04-23 21:54:42 +0200

    fix(localization): correct JSON formatting and add missing keys in language files

--- lib/features/categories/data/models/products_response/product_dto.g.dart
+++ lib/features/categories/data/models/products_response/product_dto.g.dart
@@ -7,31 +7,30 @@ part of 'product_dto.dart';
 // **************************************************************************
 
 ProductDto _$ProductDtoFromJson(Map<String, dynamic> json) => ProductDto(
-  rateAvg: (json['rateAvg'] as num?)?.toInt(),
-  rateCount: (json['rateCount'] as num?)?.toInt(),
-  title: json['title'] as String?,
-  slug: json['slug'] as String?,
-  description: json['description'] as String?,
-  imgCover: json['imgCover'] as String?,
-  images: (json['images'] as List<dynamic>?)?.map((e) => e as String).toList(),
-  price: (json['price'] as num?)?.toInt(),
-  priceAfterDiscount: (json['priceAfterDiscount'] as num?)?.toInt(),
-  quantity: (json['quantity'] as num?)?.toInt(),
-  category: json['category'] as String?,
-  occasion: json['occasion'] as String?,
-  createdAt:
-      json['createdAt'] == null
+      rateAvg: (json['rateAvg'] as num?)?.toInt(),
+      rateCount: (json['rateCount'] as num?)?.toInt(),
+      title: json['title'] as String?,
+      slug: json['slug'] as String?,
+      description: json['description'] as String?,
+      imgCover: json['imgCover'] as String?,
+      images:
+          (json['images'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      price: (json['price'] as num?)?.toInt(),
+      priceAfterDiscount: (json['priceAfterDiscount'] as num?)?.toInt(),
+      quantity: (json['quantity'] as num?)?.toInt(),
+      category: json['category'] as String?,
+      occasion: json['occasion'] as String?,
+      createdAt: json['createdAt'] == null
           ? null
           : DateTime.parse(json['createdAt'] as String),
-  updatedAt:
-      json['updatedAt'] == null
+      updatedAt: json['updatedAt'] == null
           ? null
           : DateTime.parse(json['updatedAt'] as String),
-  v: (json['__v'] as num?)?.toInt(),
-  discount: (json['discount'] as num?)?.toInt(),
-  sold: (json['sold'] as num?)?.toInt(),
-  id: json['_id'] as String?,
-);
+      v: (json['__v'] as num?)?.toInt(),
+      discount: (json['discount'] as num?)?.toInt(),
+      sold: (json['sold'] as num?)?.toInt(),
+      id: json['_id'] as String?,
+    );
 
 Map<String, dynamic> _$ProductDtoToJson(ProductDto instance) =>
     <String, dynamic>{
