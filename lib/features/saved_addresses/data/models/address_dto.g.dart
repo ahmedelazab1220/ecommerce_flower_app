008bb8336718646d9ed4072b8ce60471331576c7
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-04-23 22:16:55 +0200

    refactor(user_addresses): Simplify constructor and improve formatting in DTO and Retrofit client files

--- lib/features/saved_addresses/data/models/address_dto.g.dart
+++ lib/features/saved_addresses/data/models/address_dto.g.dart
@@ -7,14 +7,14 @@ part of 'address_dto.dart';
 // **************************************************************************
 
 AddressDto _$AddressDtoFromJson(Map<String, dynamic> json) => AddressDto(
-      id: json['_id'] as String?,
-      street: json['street'] as String?,
-      city: json['city'] as String?,
-      lat: json['lat'] as String?,
-      long: json['long'] as String?,
-      phone: json['phone'] as String?,
-      username: json['username'] as String?,
-    );
+  id: json['_id'] as String?,
+  street: json['street'] as String?,
+  city: json['city'] as String?,
+  lat: json['lat'] as String?,
+  long: json['long'] as String?,
+  phone: json['phone'] as String?,
+  username: json['username'] as String?,
+);
 
 Map<String, dynamic> _$AddressDtoToJson(AddressDto instance) =>
     <String, dynamic>{
