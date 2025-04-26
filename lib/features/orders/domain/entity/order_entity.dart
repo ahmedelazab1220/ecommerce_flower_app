bddadc36d5a112aae97fe80a1a3f3bddce02c032
Eslam <eslamm378@gmail.com>
2025-04-26 03:48:32 +0300

    feat(orders): implementing orders domain layer.

---
+++ lib/features/orders/domain/entity/order_entity.dart
@@ -0,0 +1,35 @@
+import 'package:ecommerce_flower_app/core/utils/shared_models/product_entity.dart';
+
+class OrderEntity {
+  final String orderId;
+  final String orderNumber;
+  final List<OrderItemEntity> orderItems;
+  final int totalPrice;
+  final bool isPaid;
+  final bool isDelivered;
+  final String state;
+
+  OrderEntity({
+    required this.orderId,
+    required this.orderNumber,
+    required this.orderItems,
+    required this.totalPrice,
+    required this.isPaid,
+    required this.isDelivered,
+    required this.state,
+  });
+}
+
+class OrderItemEntity {
+  final ProductEntity product;
+  final int quantity;
+  final int price;
+  final String orderItemId;
+
+  OrderItemEntity({
+    required this.product,
+    required this.quantity,
+    required this.price,
+    required this.orderItemId,
+  });
+}
