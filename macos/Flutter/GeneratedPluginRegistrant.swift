585e7376de0a8c378013e067f2a78edf57fe88b6
PeshoOReda <drmafiaemail13@gmail.com>
2025-04-30 20:43:40 +0300

    feat: Remove unused `flutter_local_notifications` package
    - Removed `flutter_local_notifications` from `pubspec.yaml` and `pubspec.lock`.
    - Removed associated generated code in `macos/Flutter/GeneratedPluginRegistrant.swift` and `windows/flutter/generated_plugins.cmake`.
    
    Signed-off-by: PeshoOReda <drmafiaemail13@gmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import file_selector_macos
 import firebase_core
 import firebase_messaging
-import flutter_local_notifications
 import flutter_secure_storage_macos
 import location
 import path_provider_foundation
@@ -20,7 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
